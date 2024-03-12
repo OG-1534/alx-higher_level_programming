a19f0715e4e94cb3abe7eb2f868036a47770ec2a
OG-1534 <gershomochanda@gmail.com>
2024-03-12 12:22:19 -0700

    class Rectangle that defines a rectangle, constructor must take 2 arguments w and h

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * class Rectangle that defines a rectangle
+ * constructor must take 2 arguments width(w) and height(h)
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}module.exports = Rectangle;
