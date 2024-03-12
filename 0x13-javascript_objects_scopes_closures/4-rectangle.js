280f76512826756bdde38d6fbdce7ce080b2af98
OG-1534 <gershomochanda@gmail.com>
2024-03-12 12:39:58 -0700

    class Rectangle that defines a rectangle, instance methods print(), rotate() and double()

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+
+/**
+ * class Rectangle that defines a rectangle
+ * constructor must take 2 arguments: w and h
+ * If w or h is equal to 0 or not a positive integer, create an empty object
+ * instance method print() prints the rectangle using the character X
+ * instance method rotate() exchanges the width and the height of the rectangle
+ * instance method double() multiples the width and the height of the rectangle by 2
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let j = 0;
+      while (j < this.width) {
+        myVar += 'X';
+        j++;
+      }
+
+      console.log(myVar);
+    }
+  }
+	rotate () {
+    let temp = 0;
+    temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
