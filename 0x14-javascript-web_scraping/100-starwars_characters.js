293066752894e03bbc858851ca58e18b2d6a5caf
OG-1534 <gershomochanda@gmail.com>
2024-04-24 14:56:29 -0700

    script that prints all characters of a Star Wars movie, first argument is the Movie ID - example: 3 = “Return of the Jedi”, Display one character name by line, must use the Star wars API, must use the module request

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
